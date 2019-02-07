ffe4bb31a53e5f906c83ceb6cc6d6019b7efcc7e
addewyd <addewyd@mail.ru>
2019-02-07 10:12:10 +0400

    accelapplied to the ball

--- app/src/main/java/com/addewyd/kot002/MainActivity.kt
+++ app/src/main/java/com/addewyd/kot002/MainActivity.kt
@@ -18,6 +18,9 @@ import android.view.View
 import android.view.View.OnTouchListener
 import android.widget.TextView
 import android.graphics.Typeface
+import android.widget.LinearLayout
+
+
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
     val sensorManager: SensorManager by lazy {
@@ -25,54 +28,40 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     }
 
     var mv:MovementView? = null
+    var tv:TextView? = null
     var gx:Float = 0f
     var gy:Float = 0f
     var gz:Float = 0f
 
-    var sensorTA: Sensor? = null
-    var sensorTGY: Sensor? = null
-    var sensorTMF: Sensor? = null
-    var sensorTGR: Sensor? = null
-    var sensorTLA: Sensor? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val linLayout = LinearLayout(this)
+        linLayout.orientation = LinearLayout.VERTICAL
+        val linLayoutParam = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT)
+        setContentView(linLayout, linLayoutParam)
+
+        val lpView = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+
+        tv = TextView(this)
+        tv?.height = 120
+        tv?.text = "TextView"
+        tv?.layoutParams = lpView
+        linLayout.addView(tv)
+
         mv = MovementView(this)
         mv?.init()
-        setContentView(mv)
+        linLayout.addView(mv)
+
+        //setContentView(mv)
     }
 
     override fun onResume() {
         super.onResume()
-        sensorTA = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
-        sensorTGY = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE)
-        sensorTMF = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
-        sensorTGR = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY)
-        sensorTLA = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION)
 
         sensorManager.registerListener(
             this,
-            sensorTA,
-            SensorManager.SENSOR_DELAY_NORMAL
-        )
-        sensorManager.registerListener(
-            this,
-            sensorTGY,
-            SensorManager.SENSOR_DELAY_NORMAL
-        )
-        sensorManager.registerListener(
-            this,
-            sensorTMF,
-            SensorManager.SENSOR_DELAY_NORMAL
-        )
-        sensorManager.registerListener(
-            this,
-            sensorTGR,
-            SensorManager.SENSOR_DELAY_NORMAL
-        )
-        sensorManager.registerListener(
-            this,
-            sensorTLA,
+            sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
             SensorManager.SENSOR_DELAY_NORMAL
         )
     }
@@ -96,8 +85,8 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         mv?.gx = gx
         mv?.gy = gy
         mv?.gz = gz
-        mv?.sn = sn
-        mv?.st = st
+
+        tv?.text = "Accelerometer %s type %d\nx %.2f\ny %.2f\nz %.2f".format(sn, st,  gx, gy, gz)
     }
 }
 
@@ -158,17 +147,9 @@ class MovementView(var ctx: Context) : SurfaceView(ctx), SurfaceHolder.Callback 
     var gx: Float = 0f
     var gy: Float = 0f
     var gz: Float = 0f
-    var sn: String = ""
-    var st: Int = 0
 
-    var ogx: Float = 0f
-    var ogy: Float = 0f
-    var ogz: Float = 0f
-    var osn: String = ""
-    var ost: Int = 0
-
-    var xVel: Int = 2
-    var yVel: Int = 2
+    var xVel: Float = 0f
+    var yVel: Float = 0f
 
     var cwidth: Int = 0
     var cheight: Int = 0
@@ -182,10 +163,11 @@ class MovementView(var ctx: Context) : SurfaceView(ctx), SurfaceHolder.Callback 
     lateinit var updateThread: UpdateThread
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
-        xPos = event!!.x
-        yPos = event.y
-        when(event.action) {
-            MotionEvent.ACTION_DOWN -> {}
+        when(event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                this.xPos = event.x
+                this.yPos = event.y
+            }
             MotionEvent.ACTION_MOVE -> {}
             MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {}
 
@@ -238,87 +220,11 @@ class MovementView(var ctx: Context) : SurfaceView(ctx), SurfaceHolder.Callback 
         brush1.setTypeface (Typeface.SANS_SERIF)
         canvas?.drawColor(Color.WHITE)
         canvas?.drawCircle(xPos, yPos, circleRadius, circlePaint)
-        var sx = "%.2f".format(gx)
-        var sy = "%.2f".format(gy)
-        var sz = "%.2f".format(gz)
-
-        var sname = ""
-        var sty = 0f
-
-        when(st) {
-            Sensor.TYPE_ACCELEROMETER -> {
-                    sname = "accelerometer"
-                sty = 0f
-            }
-            Sensor.TYPE_GRAVITY -> {
-                sname = "gravity"
-                sty = 1f
-            }
-            Sensor.TYPE_MAGNETIC_FIELD -> {
-                sname = "magnetic"
-                sty = 2f
-            }
-            Sensor.TYPE_GYROSCOPE -> {
-                sname = "gyroscope"
-                sty = 3f
-            }
-            Sensor.TYPE_LINEAR_ACCELERATION -> {
-                sname = "linear accelerometer"
-                sty = 4f
-            }
-        }
-
-        var snst = "%d Name %s - %s".format(st, sn, sname)
-        var x = 10f
-
-        canvas?.drawText(sx,  x, 20f + sty * 100, brush1)
-        canvas?.drawText(sy,  x, 40f + sty * 100, brush1)
-        canvas?.drawText(sz,  x, 60f + sty * 100, brush1)
-        canvas?.drawText(snst,x, 80f + sty * 100, brush1)
-
-        if (st != ost) {
-            when(ost) {
-                Sensor.TYPE_ACCELEROMETER -> {
-                    sname = "accelerometer"
-                    sty = 0f
-                }
-                Sensor.TYPE_GRAVITY -> {
-                    sname = "gravity"
-                    sty = 1f
-                }
-                Sensor.TYPE_MAGNETIC_FIELD -> {
-                    sname = "magnetic"
-                    sty = 2f
-                }
-                Sensor.TYPE_GYROSCOPE -> {
-                    sname = "gyroscope"
-                    sty = 3f
-                }
-                Sensor.TYPE_LINEAR_ACCELERATION -> {
-                    sname = "linear accelerometer"
-                    sty = 4f
-                }
-            }
-            var osx = "%.2f".format(ogx)
-            var osy = "%.2f".format(ogy)
-            var osz = "%.2f".format(ogz)
-
-            var snst = "%d Name %s - %s".format(ost, osn, sname)
-            var x = 10f
-
-            canvas?.drawText(osx,  x, 20f + sty * 100, brush1)
-            canvas?.drawText(osy,  x, 40f + sty * 100, brush1)
-            canvas?.drawText(osz,  x, 60f + sty * 100, brush1)
-            canvas?.drawText(snst, x, 80f + sty * 100, brush1)
-            ogx = gx
-            ogy = gy
-            ogz = gz
-            osn = sn
-            ost = st
-        }
     }
 
     fun updatePhysics() {
+        var fact: Float = 50f;
+
         xPos += xVel
         yPos += yVel
 
@@ -328,7 +234,9 @@ class MovementView(var ctx: Context) : SurfaceView(ctx), SurfaceHolder.Callback 
             } else {
                 yPos = cheight - circleRadius
             }
-            yVel *= -1
+            yVel = 0f
+        } else {
+            yVel += gy / fact
         }
         if (xPos - circleRadius < 0 || xPos + circleRadius > cwidth) {
             if (xPos - circleRadius < 0) {
@@ -336,7 +244,11 @@ class MovementView(var ctx: Context) : SurfaceView(ctx), SurfaceHolder.Callback 
             } else {
                 xPos = cwidth - circleRadius
             }
-            xVel *= -1
+            xVel = 0f
+
+        }
+        else {
+            xVel -= gx / fact
         }
     }
 }
